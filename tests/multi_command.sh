c3de28b4c9c9a4a13df57d16363b2dfefee0808b
nvold <nvold001@ucr.edu>
2017-05-09 23:31:10 -0700

    Update multi_command.sh

--- tests/multi_command.sh
+++ tests/multi_command.sh
@@ -2,10 +2,11 @@
 echo cd
 echo cd bin
 echo ./rshell
-echo ls -a ; echo hello ; mkdir test
-echo ls -l ; echo how are you && mkdir testDirec || echo world ; ls -l
-echo echo universe ; rmdir testDirect && git status ; ls -l
-echo touch newFile && echo the file has been created ; git status ; ls
-echo echo will now remove the file ; rm newFile && echo file deleted ; ls -l
-echo abc || echo previous command failed cause its not a command, so the or works
+ls -a ; echo hello ; mkdir test
+els -l ; echo how are you && mkdir testDirect || echo world ; ls -l
+echo universe ; rmdir testDirect && git status ; ls -l
+touch newFile && echo the file has been created ; git status ; ls
+echo will now remove the file ; rm newFile && echo file deleted ; ls -l
+abc || echo previous command failed cause its not a command, so the or works
+rmdir test && exit
 
