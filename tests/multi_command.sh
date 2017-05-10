7e5cff05f2e232b439e85c970f6749f4bbec1523
nvold <nvold001@ucr.edu>
2017-05-09 23:31:30 -0700

    Update multi_command.sh

--- tests/multi_command.sh
+++ tests/multi_command.sh
@@ -8,5 +8,5 @@ echo universe ; rmdir testDirect && git status ; ls -l
 touch newFile && echo the file has been created ; git status ; ls
 echo will now remove the file ; rm newFile && echo file deleted ; ls -l
 abc || echo previous command failed cause its not a command, so the or works
-rmdir test && exit
+rmdir test
 
