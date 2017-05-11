a58a3abd6c7ec4e694e6434e5448b6ef1d9b4d3b
nvold <nvold001@ucr.edu>
2017-05-10 18:22:59 -0700

    Update commented_command.sh

--- tests/commented_command.sh
+++ tests/commented_command.sh
@@ -1,9 +1,2 @@
 #!/bin/sh
-cd
-cd bin
-./rshell
-ls -l #this is a comment
-echo hello world ; ls -a #comment again
-mkdir newDirTesting ; echo the directory should be created && ls -l #another comment
-rmdir newDirTesting #getting rid of the test directory
-exit #Comment for exit
+../bin/rshell < comments.txt
