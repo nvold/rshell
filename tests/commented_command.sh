178b8109f49870e9e9240dfd280a11ac9edc46eb
nvold <nvold001@ucr.edu>
2017-05-10 17:31:05 -0700

    Update commented_command.sh

--- tests/commented_command.sh
+++ tests/commented_command.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-echo cd
-echo cd bin
-echo ./rshell
+cd
+cd bin
+./rshell
 ls -l #this is a comment
 echo hello world ; ls -a #comment again
 mkdir newDirTesting ; echo the directory should be created && ls -l #another comment
